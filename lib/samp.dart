113391b32f439211edc153b65ac836a05d2f11ba
srinu2003 <tsrin2003@gmail.com>
2024-03-30 00:39:20 +0530

    Add launch.json and comment out unused code in samp.dart

--- lib/samp.dart
+++ lib/samp.dart
@@ -1,46 +1,46 @@
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
 
-class Sample extends StatelessWidget {
-  const Sample({super.key});
+// class Sample extends StatelessWidget {
+//   const Sample({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: MyButton(onClicked: () {},child: const Text('Sample'),),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: MyButton(onClicked: () {},child: const Text('Sample'),),
+//       ),
+//     );
+//   }
+// }
 
-class MyButton extends FloatingActionButton {
-  const MyButton({super.key, required this.onClicked, required this.child})
-      : super(onPressed: onClicked, elevation: 0.0, child: child);
+// class MyButton extends FloatingActionButton {
+//   const MyButton({super.key, required this.onClicked, required this.child})
+//       : super(onPressed: onClicked, elevation: 0.0, child: child);
 
-  final VoidCallback onClicked;
-  @override
-  // ignore: overridden_fields
-  final Widget child;
+//   final VoidCallback onClicked;
+//   @override
+//   // ignore: overridden_fields
+//   final Widget child;
 
-  @override
-  Widget build(BuildContext context) {
-    return Theme(
-      data: Theme.of(context).copyWith(
-        // buttonColor: Theme.of(context).accentColor,
-        buttonTheme: Theme.of(context).buttonTheme.copyWith(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(
-                  color: Colors.white,
-                  width: 1.0,
-                  style: BorderStyle.solid,
-                ),
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
-      ),
-      child: Builder(builder: super.build),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Theme(
+//       data: Theme.of(context).copyWith(
+//         // buttonColor: Theme.of(context).accentColor,
+//         buttonTheme: Theme.of(context).buttonTheme.copyWith(
+//               shape: RoundedRectangleBorder(
+//                 side: const BorderSide(
+//                   color: Colors.white,
+//                   width: 1.0,
+//                   style: BorderStyle.solid,
+//                 ),
+//                 borderRadius: BorderRadius.circular(30),
+//               ),
+//             ),
+//       ),
+//       child: Builder(builder: super.build),
+//     );
+//   }
+// }
